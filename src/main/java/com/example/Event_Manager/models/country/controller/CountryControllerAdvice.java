cf00e5cb546530f1cb559748cbdfe1965041d132
MichalPranagal <michal.pranagal@proton.me>
2025-11-25 20:11:11 +0100

    Add CountryControllerAdvice to handle Country related errors

---
+++ src/main/java/com/example/Event_Manager/models/country/controller/CountryControllerAdvice.java
@@ -0,0 +1,25 @@
+package com.example.Event_Manager.models.country.controller;
+
+import com.example.Event_Manager.models.country.exceptions.CountryNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.example.Event_Manager.models._util.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice(basePackageClasses = CountryApi.class)
+public class CountryControllerAdvice {
+
+    @ExceptionHandler(CountryNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCountryNotFound(CountryNotFoundException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
